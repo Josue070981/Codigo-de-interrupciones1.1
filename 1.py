2a1a275c3a8a16aa8c92a283d718febb840ee135
Josue070981 <119982169+Josue070981@users.noreply.github.com>
2023-10-28 03:06:03 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,16 @@
 import random
 import time
 
-# Función para generar números aleatorios
 def generar_numeros_aleatorios(minimo, maximo):
     return random.randint(minimo, maximo)
-
-# Función para realizar una operación (resta, suma, multiplicación, división)
+    
 def operacion_numeros(operacion, tiempo_limite):
     start_time = time.time()
     print(f"Iniciando operación de {operacion} durante {tiempo_limite} segundos.")
     while time.time() - start_time < tiempo_limite:
         numero1 = generar_numeros_aleatorios(1, 100)
         numero2 = generar_numeros_aleatorios(1, 100)
-        resultado = None  # Inicializamos resultado con None
-
+        resultado = None  
         if operacion == "resta":
             resultado = numero1 - numero2
         elif operacion == "suma":
@@ -22,23 +19,18 @@ def operacion_numeros(operacion, tiempo_limite):
             resultado = numero1 * numero2
         elif operacion == "division":
             resultado = numero1 / numero2
-
         print(f"Números utilizados para {operacion}: {numero1} y {numero2}")
         # Verificar si resultado no es None antes de imprimir
         if resultado is not None:
             print(f"Resultado de {operacion}: {resultado}")
+            
+min_tiempo = 5 
+max_tiempo = 15 
 
-# Define el rango de tiempo para cada operación
-min_tiempo = 5  # Tiempo mínimo en segundos
-max_tiempo = 15  # Tiempo máximo en segundos
-
-# Realiza las operaciones en el orden deseado con tiempos aleatorios
 operacion_numeros("resta", random.uniform(min_tiempo, max_tiempo))
 operacion_numeros("suma", random.uniform(min_tiempo, max_tiempo))
 operacion_numeros("division", random.uniform(min_tiempo, max_tiempo))
 operacion_numeros("multiplicacion", random.uniform(min_tiempo, max_tiempo))
-
-# Operaciones adicionales
 operacion_numeros("divisom", random.uniform(min_tiempo, max_tiempo))
 operacion_numeros("suma", random.uniform(min_tiempo, max_tiempo))
 operacion_numeros("resta", random.uniform(min_tiempo, max_tiempo))
